eeeee36a64ae7b2ebd82acfcbb61cd2a3465692e
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-03-29 22:54:03 +0200

    Add project files.

---
+++ ChallengeApp_01/Program.cs
@@ -0,0 +1,13 @@
+﻿List<string> dayOfWeek = new List<string>();
+dayOfWeek.Add("Poniedziałek");
+dayOfWeek.Add("Wtorek");
+dayOfWeek.Add("Środa");
+dayOfWeek.Add("Czwartek");
+dayOfWeek.Add("Piątek");
+dayOfWeek.Add("Sobota");
+dayOfWeek.Add("Niedziela");
+
+for (int i = 0; i < dayOfWeek.Count; i++)
+{
+    Console.WriteLine(dayOfWeek[i]);
+}
