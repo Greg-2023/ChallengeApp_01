6a3dedf97c32f6f863600b27ae4c04d23894d0f8
Greg-2023 <grzegorz.tomasz.badura@gmail.com>
2023-04-01 00:15:16 +0200

    Zadanie domowe dzień 6

--- ChallengeApp_01/Program.cs
+++ ChallengeApp_01/Program.cs
@@ -1,13 +1,62 @@
-﻿List<string> dayOfWeek = new List<string>();
-dayOfWeek.Add("Poniedziałek");
-dayOfWeek.Add("Wtorek");
-dayOfWeek.Add("Środa");
-dayOfWeek.Add("Czwartek");
-dayOfWeek.Add("Piątek");
-dayOfWeek.Add("Sobota");
-dayOfWeek.Add("Niedziela");
+﻿using ChallengeApp_01;
 
-for (int i = 0; i < dayOfWeek.Count; i++)
+Employee user1 = new Employee("Grzegorz", "Badura", 45);
+Employee user2 = new Employee("Mateusz", "Badura", 15);
+Employee user3 = new Employee("Mariusz", "Ostapowicz", 47);
+
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(1);
+user1.AddScore(8);
+user1.AddScore(1);
+user1.AddScore(3);
+// sum is 20
+
+user2.AddScore(1);
+user2.AddScore(3);
+user2.AddScore(4);
+user2.AddScore(9);
+user2.AddScore(7);
+user2.AddScore(1);
+// sum is 25
+
+user3.AddScore(10);
+user3.AddScore(10);
+user3.AddScore(10);
+user3.AddScore(10);
+user3.AddScore(1);
+user3.AddScore(10);
+// sum is 51
+
+var result1 = user1.Result;
+var result2 = user2.Result;
+var result3 = user3.Result;
+string nameMax = "";
+string surnameMax = "";
+var resultMax = 0;
+
+Console.WriteLine("Pracownik nr 1 to: " + user1.Name + " " + user1.Surname + ", lat " + user1.Age + ", jego punkty to: " + user1.Result);
+Console.WriteLine("Pracownik nr 2 to: " + user2.Name + " " + user2.Surname + ", lat " + user2.Age + ", jego punkty to: " + user2.Result);
+Console.WriteLine("Pracownik nr 3 to: " + user3.Name + " " + user3.Surname + ", lat " + user3.Age + ", jego punkty to: " + user3.Result);
+
+if (user1.Result > user2.Result)
 {
-    Console.WriteLine(dayOfWeek[i]);
+    resultMax = user1.Result;
+    nameMax = user1.Name;
+    surnameMax = user1.Surname;
 }
+else if (user2.Result > user3.Result)
+{
+    resultMax = user2.Result;
+    nameMax = user2.Name;
+    surnameMax = user2.Surname;
+}
+else if (user3.Result > user2.Result)
+{
+    resultMax = user3.Result;
+    nameMax = user3.Name;
+    surnameMax = user3.Surname;
+}
+
+Console.WriteLine();
+Console.WriteLine("Pracownik z największą liczbą punktów to: " + nameMax + " " + surnameMax + ", a jego liczba punktów to: " + resultMax);
